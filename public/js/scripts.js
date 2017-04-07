41ab8d4505cd9d61de0e8461fc899356d2911e2a
avravikiran-sopho <avenkataravikiran@gmail.com>
2017-04-07 13:46:02 +0000

    js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,7 +2,23 @@
  * scripts.js
  *
  * Computer Science 50
- * Problem Set 7
+ * Problem Set
  *
  * Global JavaScript, if any.
  */
+
+document.onload = function() {myMove()};
+function myMove() {
+  var elem = document.getElementsByClassName("product-top");   
+  var pos = 50;
+  var id = setInterval(frame, 5);
+  function frame() {
+    if (pos == 1) {
+      clearInterval(id);
+    } else {
+      pos--; 
+      elem.style.top = pos + '%';
+      elem.style.opacity = 1 - (pos-1)/100;
+    }
+  }
+}
